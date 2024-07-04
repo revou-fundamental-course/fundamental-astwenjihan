356adcf008786425f245a440591ad9cff1d77c8f
Jihan Astwen <astwenjihan@gmail.com>
2024-07-04 22:51:27 +0700

    config website

--- js/script.js
+++ js/script.js
@@ -7,8 +7,25 @@ function validateForm() {
     console.log(TanggalLahir);
 
     if (Nama != '' && TanggalLahir != '') {
-        document.querySelector('#result').innerHTML = Nama + ' ' + TanggalLahir;
+        document.querySelector('#result').innerHTML = Nama + ' ' + TanggalLahir + ' ' + Pesan;
     } else {
         alert('Inputan ada yang kosong');
     }
 }
+
+function replaceName() {
+    let name = prompt("Halo, Siapa nama anda?", "");
+    document.getElementById("name").innerHTML = name
+}
+
+document.getElementById('tombol').addEventListener("click", function() {
+    replaceName()
+})
+
+const menuIcon = document.getElementById("menu-icon");
+const menuList = document.getElementById("menu-list");
+
+menuIcon.addEventListener("click", () => {
+  menuList.classList.toggle("hidden");
+  alert("klik ikon")
+});
