fcd7813312ebe2121e45bf8422e98601d326f7c0
Jihan Astwen <astwenjihan@gmail.com>
2024-07-04 16:37:55 +0700

    config website

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
 // Ini Javascript //
-
 function validateForm() {
     let Nama = document.getElementById('nama').value;
     let TanggalLahir = document.getElementById('tanggallahir').value;
@@ -12,27 +11,4 @@ function validateForm() {
     } else {
         alert('Inputan ada yang kosong');
     }
-}
-
-let indexSlide = 1; // [0, 1, 2]
-showSlide(1);
-
-function nextSlide(n) {
-    showSlide(indexSlide += n);
-    console.log('IndexSlide kita tambah ' + n);
-    console.log(indexSlide + 'Ini NextSlide');
-}
-
-function showSlide(n) {
-    let listImage = document.getElementsByClassName('banner-item');
-    if (n > listImage.length) indexSlide = 1;
-
-    let index = 0;
-    while (index < listImage.length) {
-        listImage[index].style.display = 'none';
-        index++;
-    }
-
-    listImage[indexSlide - 1].style.display = 'block';
-    console.log(indexSlide - 1 + 'Ini ShowSlide');
 }
